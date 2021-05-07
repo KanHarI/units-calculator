16099e868cdb85b01ef840e8739382516edd1bc3
Itay Knaan Harpaz <knaan.harpaz@gmail.com>
2021-05-07 22:27:57 +0300

    Add unitles numbers and addition of units

--- tests/test_base_arithmetic.py
+++ tests/test_base_arithmetic.py
@@ -1,6 +1,27 @@
+import pytest
+
 from units_calculator.si_units import Seconds
+from units_calculator.units_bases import Number
+
+
+def test_create_unitless_number():
+    n = Number(5.0)
+    assert repr(n) == "5.0"
 
 
 def test_create_direct_unit():
     s = Seconds(5.0)
     assert repr(s) == "5.0s"
+
+
+def test_add_units():
+    s1 = Seconds(4.0)
+    s2 = Seconds(3)
+    assert repr(s1 + s2) == "7.0s"
+
+
+def test_add_unit_and_number():
+    s = Seconds(1.0)
+    n = Number(-3)
+    with pytest.raises(AssertionError):
+        n += s
--- units_calculator/units_bases.py
+++ units_calculator/units_bases.py
@@ -4,9 +4,6 @@ from __future__ import annotations
 
 from typing import Any, Type, cast
 
-# A list of dimensions and their exponents
-Dimensions = list[tuple[int, int]]
-
 
 class UnitsMeta(type):
     """A metaclass for all unit classes"""
@@ -21,8 +18,9 @@ class UnitsMeta(type):
             raise RuntimeError(f"{symbol} defined twice!")
         if is_base_unit:
             idx = len(dimensions_dict)
-            namespace["__dimensions__"] = [(idx, 1)]
+            namespace["__dimensions__"] = list()
             result = cast(UnitsMeta, super().__new__(cls, name, bases, namespace))
+            namespace["__dimensions__"].append((idx, 1, result))
             dimensions_dict[symbol] = idx, result
             idx_to_dimension[idx] = symbol, result
         else:
@@ -33,25 +31,56 @@ class UnitsMeta(type):
 dimensions_dict: dict[str, tuple[int, UnitsMeta]] = dict()
 idx_to_dimension: dict[int, tuple[str, UnitsMeta]] = dict()
 
+# A list of dimensions and their exponents
+Dimensions = list[tuple[int, int, UnitsMeta]]
+
 
 class Unit(metaclass=UnitsMeta):
     """A base class for all units"""
-
-    __dimensions__: Dimensions
-    __symbol__: str
 
     def __init__(self, numerical_val: complex, dimensions: Dimensions):
         self._numerical_val: complex = numerical_val
         self._dimensions: Dimensions = dimensions
 
+    def _is_matching_dimensions(self, other: Unit) -> bool:
+        if len(self._dimensions) != len(other._dimensions):
+            return False
+        for dimension1, dimension2 in zip(self._dimensions, other._dimensions):
+            idx1, exp1, _ = dimension1
+            idx2, exp2, _ = dimension2
+            if (idx1, exp1) != (idx2, exp2):
+                return False
+        return True
+
+    def _clone(self) -> Unit:
+        return Unit(self._numerical_val, self._dimensions)
+
+    def __iadd__(self, other: Unit) -> Unit:
+        assert self._is_matching_dimensions(other)
+        self._numerical_val += other._numerical_val
+        return self
+
+    def __add__(self, other: Unit) -> Unit:
+        new_unit = Unit(self._numerical_val, self._dimensions)
+        new_unit += other
+        return new_unit
+
     def __repr__(self):
-        return repr(self._numerical_val) + self.__symbol__
+        numerical_representation = repr(self._numerical_val)
+        units_representation = ""
+        for _, exp, unit in self._dimensions:
+            units_representation += unit.__symbol__
+            if exp != 1:
+                units_representation += f"^{str(exp) if exp>0 else '(' + str(exp) + ')'}"
+        return numerical_representation + units_representation
 
 
 class BaseUnit(Unit):
     """A class for base units"""
 
-    __base_unit__: bool = True
+    __dimensions__: Dimensions
+    __symbol__: str
+    __multiplier__: complex = 1
 
     def __init__(self, numerical_val: complex):
         super().__init__(numerical_val, self.__dimensions__)
@@ -60,4 +89,13 @@ class BaseUnit(Unit):
 class DerivedUnit(Unit):
     """A base class for derived units"""
 
-    __base_unit__: bool = False
+    __base_unit__: Type[UnitsMeta]
+    __symbol__: str
+    __multiplier__: complex
+
+
+class Number(Unit):
+    """A class for unitless numbers"""
+
+    def __init__(self, numerical_val: complex):
+        super().__init__(numerical_val, list())
